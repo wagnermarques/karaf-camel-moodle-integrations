aafb9a3d976add86cca657ee26246e0d039bef5b
wagnermarques <wagnerdocri@gmail.com>
2025-04-06 02:27:59 -0300

    ajust moodledata permission

--- bin/docker-compose-up.sh
+++ bin/docker-compose-up.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+sudo groupadd --gid 33 www-data
+sudo useradd --uid 33 --gid www-data --no-create-home --shell /sbin/nologin www-data
+
 cd ./../containers
 docker-compose -f docker-compose.yml up -d --build
